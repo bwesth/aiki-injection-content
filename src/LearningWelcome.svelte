a4c6d7197b12493cb8be0acbdfb8221a83d43ffd
John Henrik Muller <jhmu@itu.dk>
2021-04-29 11:43:08 +0200

    Added Aiki logo and global css values.

--- src/LearningWelcome.svelte
+++ src/LearningWelcome.svelte
@@ -13,6 +13,7 @@
 {#if render}
 <div on:click={removeGreeting} class="aiki-overlay" transition:fade="{{delay: 0, duration: 1000}}">
   <div class="aiki-wrapper">
+    <img src="images/AikiLogo.png" alt="Aiki Logo"/>
     <p>Let's do something productive! 😊</p>
   </div>
 </div>
@@ -30,26 +31,31 @@
   }
 
   .aiki-wrapper {
+    background-color: var(--backgroundColor);
+    border: 2px solid var(--borderColor);
     display: flex;
     flex-direction: column;
     width: 200px;
     position: fixed;
     top: 40%;
     left: calc(50% - 100px);
-    background-color: #FFFFFF;
     justify-items: center;
     align-items: center;
     border-radius: 15px;
-    box-shadow: 2px 2px 2px rgba(128, 128, 128, 0.3);
-    border: 2px solid #3278B4;
+    box-shadow: 2px 2px 2px var(--shadowColor);
+  }
+
+  img {
+    height: 2em;
   }
 
   p {
-    font-size: 16px;
+    font-size: var(--fontSize);
+    font-family: var(--fontContent);
     padding: 10px 0px;
     text-align: center;
     margin: 0;
-    color: #212121;
+    color: var(--textColor);
   }
 
 </style>
