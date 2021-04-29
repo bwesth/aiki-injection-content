416f847cc0b51c4be825e0f98228176d44b74489
John Henrik Muller <jhmu@itu.dk>
2021-04-29 14:12:05 +0200

    Preparing to work on countdown timer now.

--- src/main.js
+++ src/main.js
@@ -4,59 +4,59 @@ import LearningWelcome from "./LearningWelcome.svelte";
 function removeInfowarning() {
   // will be replaced
 }
-let timer;
+//let timer;
 
 
-// function resolve(arg) {console.log(timer.time)}
-// let timer = {
-//   time: 5000,
-//   interval: undefined,
-//   slowed: false,
-//   print: function () {},
-//   start: function () {
-// 	  console.log("Starting", timer.time)
-//     timer.interval = setInterval(() => {
-// 		console.log(Math.round(timer.time/1000))
-//       if (timer.slowed) {
-//         timer.time -= 20;
-//       } else {
-//         timer.time -= 100;
-//       }
-//       if (timer.time <= 0) {
-// 		timer.stop();
-//         resolve({ msg: "Auto resolve", removeWarning: false });
-//         // location.href = request.url;
-//       }
-//     }, 100);
-//   },
-//   stop: function () {
-//     if (timer.interval) clearInterval(timer.interval);
-//     timer.interval = undefined;
-//   },
-//   slow: function () {
-//     timer.slowed = true;
-//   },
-//   hasten: function () {
-//     timer.slowed = false;
-//   },
-// };
+function resolve(arg) {console.log(timer.time)}
+let timer = {
+  time: 5000,
+  interval: undefined,
+  slowed: false,
+  print: function () {},
+  start: function () {
+	  console.log("Starting", timer.time)
+    timer.interval = setInterval(() => {
+		console.log(Math.round(timer.time/1000))
+      if (timer.slowed) {
+        timer.time -= 20;
+      } else {
+        timer.time -= 100;
+      }
+      if (timer.time <= 0) {
+		timer.stop();
+        resolve({ msg: "Auto resolve", removeWarning: false });
+        // location.href = request.url;
+      }
+    }, 100);
+  },
+  stop: function () {
+    if (timer.interval) clearInterval(timer.interval);
+    timer.interval = undefined;
+  },
+  slow: function () {
+    timer.slowed = true;
+  },
+  hasten: function () {
+    timer.slowed = false;
+  },
+};
 
-// const procrastinationWarning = new ProcrastinationWarning({
-//   target: document.body,
-//   props: { removeInfowarning, timer },
-// });
+const procrastinationWarning = new ProcrastinationWarning({
+  target: document.body,
+  props: { removeInfowarning, timer },
+});
 
-// function removeGreeting() {
-//   const element = document.getElementsByClassName("aiki-overlay")[0];
-//   element.remove();
-// }
+//function removeGreeting() {
+//  const element = document.getElementsByClassName("aiki-overlay")[0];
+//  element.remove();
+//}
 
 let removeGreeting;
 
-const learningWelcome = new LearningWelcome({
-  target: document.body,
-  props: { removeGreeting },
-});
+// const learningWelcome = new LearningWelcome({
+//   target: document.body,
+//   props: { removeGreeting },
+// });
 
-// export default procrastinationWarning;
-export default learningWelcome;
+export default procrastinationWarning;
+// export default learningWelcome;
