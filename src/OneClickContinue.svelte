61c2e6eeab896f21c784770dac2d8169c5c2634f
John Henrik Muller <jhmu@itu.dk>
2021-05-08 15:04:32 +0200

    Styling fixes!

--- src/OneClickContinue.svelte
+++ src/OneClickContinue.svelte
@@ -15,77 +15,102 @@
   }
 </script>
 
-<div class="aiki-overlay" transition:fade={{ delay: 0, duration: 200 }}>
-  <div class="aiki-header">
-    <img class="aiki-img" src="images/AikiLogo.png" alt="Aiki Logo" />
-    <h5>Aiki<sup>3</sup></h5>
-    <div class="close-container" on:click={close}>
-      <div class="leftright" />
-      <div class="rightleft" />
+<div id="aiki-overlay" transition:fade={{ delay: 0, duration: 200 }}>
+  <div id="aiki-header">
+    <img id="aiki-img" src="images/AikiLogo.png" alt="Aiki Logo" />
+    <h5 id="aiki-h5">Aiki<sup>3</sup></h5>
+    <div id="aiki-close-container" on:click={close}>
+      <div id="aiki-leftright" />
+      <div id="aiki-rightleft" />
     </div>
   </div>
-  <button disabled={!canContinue} on:click={gotoOrigin}
-    ><p class="aiki-p">Continue</p></button
+  <button id="aiki-button" disabled={!canContinue} on:click={gotoOrigin}
+    ><p id="aiki-p">Continue</p></button
   >
 </div>
 
 <style>
-  .aiki-overlay {
+  #aiki-overlay {
     border-radius: 10px;
     flex-direction: column;
     top: 1em;
     right: 1em;
-    height: 62px;
-    width: 8em;
-    background-color: rgb(211, 211, 211);
+    height: 8vh;
+    width: 16vh;
+    background-color: #282c34;
     position: fixed;
-    z-index: 9000;
+    z-index: 9001;
     display: flex;
     align-items: flex-end;
-    padding-top: 4px;
+    padding-top: 3px;
+    box-shadow: 2px 2px 2px rgba(128, 128, 128, 0.3);
+    border: 1px solid #6c757d;
   }
 
-  button {
+  #aiki-h5 {
+    color: #ffffff;
+    font-size: 16px;
+    font-family: "Roboto", sans-serif;
+    margin: 0 auto;
+  }
+
+  #aiki-button {
     width: 100%;
+    height: 100%;
     margin: 0;
     padding: 0;
-    border-radius: 0 0 10px 10px;
+    border-radius: 0 0 9px 9px;
     display: flex;
     justify-content: center;
     z-index: 9001;
+    background-color: #28a745;
+    border-color: #28a745;
+    border: 1px solid transparent;
   }
 
-  .aiki-header {
+  #aiki-button:hover {
+    background-color: #208934;
+    border-color: #208934;
+  }
+
+  #aiki-button:disabled {
+    background-color: #6c757d;
+    border-color: #6c757d;
+  }
+
+  #aiki-button:disabled:hover {
+    background-color: #6c757d;
+    border-color: #6c757d;
+  }
+
+  #aiki-header {
     display: flex;
     width: 100%;
     justify-content: space-between;
   }
 
-  h5 {
-    margin: 0;
-    margin: 0 auto;
-  }
-
-  .aiki-img {
+  #aiki-img {
     margin-left: 3px;
-    width: 20px;
-    height: 20px;
+    width: 16px;
+    height: 16px;
     background-color: #282c34;
     border-radius: 5px;
     border: 2px solid #282c34;
   }
 
-  .aiki-p {
+  #aiki-p {
     display: flex;
     align-items: center;
     height: 4vh;
-    font-family: "Lato", sans-serif;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
     text-align: center;
     margin: 0;
-    color: #444444;
+    color: #ffffff;
+    font-weight: 400;
   }
 
-  .close-container {
+  /* Rules for the close button */
+  #aiki-close-container {
     width: 20px;
     height: 20px;
     position: relative;
@@ -94,34 +119,34 @@
     margin-right: 3px;
   }
 
-  .leftright {
+  #aiki-leftright {
     margin-top: 8px;
     height: 4px;
-    width: 20px;
+    width: 16px;
     position: absolute;
-    background-color: #f4a259;
+    background-color: grey;
     border-radius: 2px;
     transform: rotate(45deg);
     transition: all 0.3s ease-in;
   }
 
-  .rightleft {
+  #aiki-rightleft {
     margin-top: 8px;
     height: 4px;
-    width: 20px;
+    width: 16px;
     position: absolute;
-    background-color: #f4a259;
+    background-color: grey;
     border-radius: 2px;
     transform: rotate(-45deg);
     transition: all 0.3s ease-in;
   }
 
-  .close-container:hover .leftright {
+  #aiki-close-container:hover #aiki-leftright {
     transform: rotate(-45deg);
-    background-color: #f25c66;
+    background-color: #dc3545;
   }
-  .close-container:hover .rightleft {
+  #aiki-close-container:hover #aiki-rightleft {
     transform: rotate(45deg);
-    background-color: #f25c66;
+    background-color: #dc3545;
   }
 </style>
